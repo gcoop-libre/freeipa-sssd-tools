c77ecff9acea31b508eb1485385e2103d549eb0a
Damian Sartori <damian@gcoop.coop>
2022-02-17 15:24:33 -0300

    ipa-api-syn/api/query.py: changed get method to accept a new parameter to return the n last records from processed database and return a json list

--- ipa-api-syn/api/query.py
+++ ipa-api-syn/api/query.py
@@ -8,7 +8,7 @@ import random
 import datetime
 
 class Query(Resource):
-    def get(self, userid):
+    def get(self, userid, recordnum = 1):
         sqlqry = "SELECT * FROM accounts WHERE account=? ORDER BY timestamp DESC"
         current_app.config.from_pyfile('config/settings.py')
         logging.basicConfig(filename=current_app.config["LOGFILE"], 
@@ -20,24 +20,31 @@ class Query(Resource):
         with sqlite3.connect(current_app.config["DBPROCS"]) as con:
             cur = con.cursor()
             cur.execute(sqlqry,(userid,))
-            rec = cur.fetchone()
-            if rec:
-                logging.info("op=qry acct="+userid+" status=success")
+            rec = cur.fetchmany(recordnum)
+            return_list = []
+            count = 0
+            for row in rec:
+                count += 1
+                return_list.append({
+                    'item':str(count),
+                    'hash':row[0],
+                    'timestamp':datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S'),
+                    'account':row[2],
+                    'dequeue_time':datetime.datetime.fromtimestamp(row[3]).strftime('%Y-%m-%d %H:%M:%S'),
+                    'finish_time':datetime.datetime.fromtimestamp(row[4]).strftime('%Y-%m-%d %H:%M:%S'),
+                    'result':row[5],
+                    })
+
+            if len(return_list) > 0:
+                logging.info("op=qry acct="+userid+" records="+str(recordnum)+" status=success")
+                ''' return_list = "["+return_list+"]" '''
                 return jsonify({'retval':"OK",
-                                'hash':rec[0],
-                                'timestamp':datetime.datetime.fromtimestamp(rec[1]).strftime('%Y-%m-%d %H:%M:%S'),
-                                'account':rec[2],
-                                'dequeue_time':datetime.datetime.fromtimestamp(rec[3]).strftime('%Y-%m-%d %H:%M:%S'),
-                                'finish_time':datetime.datetime.fromtimestamp(rec[4]).strftime('%Y-%m-%d %H:%M:%S'),
-                                'result':rec[5]
+                                'items':str(count),
+                                'result':return_list
                                 })
+
             else:
                 logging.warning("op=qry acct="+userid+" status=account not found")
                 return jsonify({'retval':"Error",
-                                'hash':"-",
-                                'timestamp':"-",
-                                'account':"-",
-                                'dequeue_time':"-",
-                                'finish_time':"-",
                                 'result':"Account not found"
                                 })
