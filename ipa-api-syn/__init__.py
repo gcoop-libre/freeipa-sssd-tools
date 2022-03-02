95dc354656ffc9156daa22f12e279da21e76fa0a
Damian Sartori <damian@gcoop.coop>
2022-03-02 15:04:06 -0300

    ipa-api-syn/__init__.py: initialize API without assigning the object as it's not being used

--- ipa-api-syn/__init__.py
+++ ipa-api-syn/__init__.py
@@ -6,7 +6,7 @@ from flask_restful import Api, reqparse, abort, Resource
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    api = Api(app)
+    Api(app)
     with app.app_context():
         app.config.from_pyfile("config/settings.py")
 
