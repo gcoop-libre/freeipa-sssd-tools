faa5b646d0ccb1e0cb58105244e9026d4c913b38
Damian Sartori <damian@gcoop.coop>
2022-03-02 15:09:27 -0300

    ipa-api-syn/__init__.py: Changed comment string to docstring

--- ipa-api-syn/__init__.py
+++ ipa-api-syn/__init__.py
@@ -4,7 +4,9 @@ from flask_restful import Api, reqparse, abort, Resource
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    """
+    Create and configure the app
+    """
     app = Flask(__name__, instance_relative_config=True)
     Api(app)
     with app.app_context():
