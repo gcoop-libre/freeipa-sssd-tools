d123300df53429e71b4eb309d1dd70b38f790935
Damian Sartori <damian@gcoop.coop>
2022-03-02 15:14:14 -0300

    ipa-api-syn/__init__.py: test_config parameter was not used, use default settings file if none otherwise use the comming from test_config

--- ipa-api-syn/__init__.py
+++ ipa-api-syn/__init__.py
@@ -10,6 +10,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     Api(app)
     with app.app_context():
-        app.config.from_pyfile("config/settings.py")
+        if test_config is None:
+            app.config.from_pyfile("config/settings.py")
+        else:
+            app.config.from_pyfile(test_config)
 
     return app
