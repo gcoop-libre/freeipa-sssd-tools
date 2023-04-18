3f88b8b193b456761ad71c01038c22b86a4b19a2
OSiRiS <osiris@gcoop.coop>
2023-04-18 12:08:03 -0300

    ipa-api-syn/ipa-que-syn: add column action in ipa-api-syn.py, define IPA_SSS_SYN_LAST=0 when action = resyn
      @ 00:55 hs

--- ipa-api-syn/ipa-que-syn.py
+++ ipa-api-syn/ipa-que-syn.py
@@ -11,7 +11,7 @@ from yaml import load, dump, Loader, Dumper
 
 get_acct_qry = "SELECT * FROM accounts ORDER BY timestamp ASC LIMIT 1"
 del_acct_qry = "DELETE FROM accounts WHERE hash=?"
-ins_acct_qry = "INSERT INTO accounts(hash,timestamp,account,dequeue_time,finish_time,return) VALUES(?,?,?,?,?,?)"
+ins_acct_qry = "INSERT INTO accounts(hash,timestamp,account,action,dequeue_time,finish_time,return) VALUES(?,?,?,?,?,?,?)"
 upd_acct_qry = "UPDATE accounts SET finish_time=?, return=? WHERE hash=?"
 
 if len(sys.argv) < 2:
@@ -37,13 +37,12 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logging.info("op=quesyn status=started")
-if escpriv:
-    script = "sudo " + script
 
 while True:
     row_hash = ""
     row_time = ""
     row_acct = ""
+    row_actn = ""
     with sqlite3.connect(db_input) as con_input:
         cur = con_input.cursor()
         cur.execute(get_acct_qry)
@@ -52,18 +51,28 @@ while True:
             row_hash = rec[0]
             row_time = rec[1]
             row_acct = rec[2]
+            row_actn = rec[3]
+            script_prefix = ""
 
     if row_hash != "":
         logging.info("op=quesyn acct=" + row_acct + " status=processing")
         with sqlite3.connect(db_processed) as con_proc:
             con_proc.execute(
-                ins_acct_qry, (row_hash, row_time, row_acct, time.time(), 0, "")
+                ins_acct_qry, (row_hash, row_time, row_acct, row_actn, time.time(), 0, "")
             )
             con_proc.commit()
 
-        logging.info("op=quesyn acct=" + row_acct + " status=account_inserted")
+        logging.info("op=quesyn acct=" + row_acct + " row_actn=" + row_actn + " status=account_inserted")
+
+        if row_actn == 'resyn':
+            script_prefix = 'IPA_SSS_SYN_LAST=0 '
+
+        if escpriv:
+            script_prefix = "sudo " + script_prefix
+
+        script_command = script_prefix + " " + script + " " + row_acct
         process = subprocess.Popen(
-            "IPA_SSS_SYN_SHOW=0 " + script + " " + row_acct,
+            script_command,
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -71,9 +80,14 @@ while True:
         stdout, stderr = process.communicate()
         rc = process.returncode
         result = "stdout=" + stdout + " stderr=" + stderr
+        logging.info("script_command=" + script_command)
         logging.info(
             "op=quesyn acct="
             + row_acct
+            + " row_actn="
+            + row_actn
+            + " "
+            + script_prefix
             + " status=run_script  exit_code="
             + str(rc)
             + " "
